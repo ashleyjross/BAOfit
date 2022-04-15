9f4dfdd18e7918a430d28dc57d84b9216e681a45
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-15 15:34:17 -0400

    Update fitDESI_EZmockv1_1d.py

--- scripts/fitDESI_EZmockv1_1d.py
+++ scripts/fitDESI_EZmockv1_1d.py
@@ -187,7 +187,7 @@ plt.ylabel(r'$\Delta\chi^{2}$')
 plt.legend()
 plt.show()
 
-plt.errorbar(rl,rl**2.*xid,rl**2*diag,fmt='ro')
+plt.errorbar(rl,rl**2.*xiave,rl**2*diag,fmt='ro')
 fmod = outdir+'LRGEZxiave'+str(zmin)+str(zmax)+sbaotemp+str(bs)+'.dat'
 mod = np.loadtxt(fmod).transpose()
 plt.plot(mod[0],mod[0]**2.*mod[1],'k-')
